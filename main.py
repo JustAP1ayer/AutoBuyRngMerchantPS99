fc6b0d1a18156d48e456caffe6a5977ef87690e3
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2024-08-18 22:35:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ except ImportError:
     os.system("pip install opencv-python")
     os.system("pip install pydirectinput")
 
-autoMove = False # Automatically moves
+autoMove = False # Automatically moves for multi hatch
 timeOut = 0
 while True:
     buy = pyautogui.locateCenterOnScreen('buy.png', confidence=0.725)
-    if buy:
-        x, y = buy
+    buy2 = pyautogui.locateCenterOnScreen('buy2.png', confidence=0.725)
+    buy3 = pyautogui.locateCenterOnScreen('buy3.png', confidence=0.725)
+
+    if buy or buy2 or buy3:
+        x, y = buy or buy2 or buy3
         pydirectinput.moveTo(x, y)
         pydirectinput.move(5, None)
         pyautogui.mouseDown()
@@ -24,14 +27,14 @@ while True:
         pyautogui.mouseUp()
         pydirectinput.click()
         pyautogui.click(clicks=1)
-        print(f"Clicked buy button on {buy}")
+        print(f"Clicked buy button on {x, y}")
         timeOut += 0.1
     if timeOut >= 30:
         timeOut = 0
         pydirectinput.keyDown('s')
         time.sleep(1.2)
         pydirectinput.keyUp('s')
-        time.sleep(0.01)
+        time.sleep(0.1)
         pydirectinput.keyDown('w')
         time.sleep(1.2)
         pydirectinput.keyUp('w')
