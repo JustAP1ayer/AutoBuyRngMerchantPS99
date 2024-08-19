828d1a8e7bd15e7e4ce46d15e26ce921328fa68b
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2024-08-18 22:23:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
+import time
 try:
     import pydirectinput
-    import pyautogui, time
+    import pyautogui
     # pip install opencv-python
 except ImportError:
     os.system("pip install pyautogui")
     os.system("pip install opencv-python")
     os.system("pip install pydirectinput")
 
-
-
+autoMove = False # Automatically moves
+timeOut = 0
 while True:
     buy = pyautogui.locateCenterOnScreen('buy.png', confidence=0.725)
     if buy:
@@ -24,5 +25,17 @@ while True:
         pydirectinput.click()
         pyautogui.click(clicks=1)
         print(f"Clicked buy button on {buy}")
-    time.sleep(0.1)
+        timeOut += 0.1
+    if timeOut >= 30:
+        timeOut = 0
+        pydirectinput.keyDown('s')
+        time.sleep(1.2)
+        pydirectinput.keyUp('s')
+        time.sleep(0.01)
+        pydirectinput.keyDown('w')
+        time.sleep(1.2)
+        pydirectinput.keyUp('w')
     
+    timeOut += 0.2
+    print(timeOut)
+    time.sleep(0.1)
