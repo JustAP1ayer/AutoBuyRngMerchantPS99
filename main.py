77bf4533e54549f347c0087dd5ff35ae3b7f7535
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2024-08-21 13:23:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ except ImportError:
     os.system("pip install opencv-python")
     os.system("pip install pydirectinput")
 
-autoMove = False # Automatically moves for multi hatch
+autoMove = True # Automatically moves
 timeOut = 0
 while True:
-    buy = pyautogui.locateCenterOnScreen('buy.png', confidence=0.725)
-    buy2 = pyautogui.locateCenterOnScreen('buy2.png', confidence=0.725)
-    buy3 = pyautogui.locateCenterOnScreen('buy3.png', confidence=0.725)
+    buy = pyautogui.locateCenterOnScreen('buy.png', confidence=0.7)
+    buy2 = pyautogui.locateCenterOnScreen('buy2.png', confidence=0.7)
+    buy3 = pyautogui.locateCenterOnScreen('buy3.png', confidence=0.7)
+    ok = pyautogui.locateCenterOnScreen('ok.png', confidence=0.7)
+    ok2 = pyautogui.locateCenterOnScreen('ok2.png', confidence=0.7)
 
-    if buy or buy2 or buy3:
-        x, y = buy or buy2 or buy3
+    if buy or buy2 or buy3 or ok or ok2:
+        x, y = buy or buy2 or buy3 or ok or ok2
         pydirectinput.moveTo(x, y)
         pydirectinput.move(5, None)
         pyautogui.mouseDown()
