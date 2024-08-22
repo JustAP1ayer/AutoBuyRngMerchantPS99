735356ef77b11ad773c707892182ef67f176f695
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2024-08-22 15:57:44 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,24 +9,26 @@ except ImportError:
     os.system("pip install opencv-python")
     os.system("pip install pydirectinput")
 
-autoMove = False # Automatically moves
+autoMove = False # Automatically moves for multiple dice hatch
 timeOut = 0
 
 while True:
     buy = pyautogui.locateCenterOnScreen('buy.png', confidence=0.7)
     buy2 = pyautogui.locateCenterOnScreen('buy2.png', confidence=0.7)
     buy3 = pyautogui.locateCenterOnScreen('buy3.png', confidence=0.7)
+    buy4 = pyautogui.locateCenterOnScreen('buy3.png', confidence=0.7)
+
     ok = pyautogui.locateCenterOnScreen('ok.png', confidence=0.7)
     ok2 = pyautogui.locateCenterOnScreen('ok2.png', confidence=0.7)
 
-    if buy or buy2 or buy3 or ok or ok2:
-        x, y = buy or buy2 or buy3 or ok or ok2
+    if buy or buy2 or buy3 or buy4 or ok or ok2:
+        x, y = buy or buy2 or buy3 or buy4 or ok or ok2
         pydirectinput.moveTo(x, y)
         pydirectinput.move(5, None)
         pyautogui.mouseDown()
         pydirectinput.click()
         pyautogui.click(clicks=1)
-        print(f"Clicked buy button on {x, y}")
+        print(f"Clicked button on {x, y}")
         if autoMove == True:
             timeOut += 0.1
     if timeOut >= 30 and autoMove == True:
@@ -42,4 +44,3 @@ while True:
         timeOut += 0.2
         print(timeOut)
     time.sleep(0.1)
-
